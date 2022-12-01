6667c3a3d3e00522e1440e7e1953f796d7858822
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2022-12-01 10:38:44 -0700

    add no-op processor + example sources (#2)
    main
    
    - Add `ImmutableProcessor`, which currently only implements boilerplate
    logic.
    
    test
    
    - Add `TestCompiler`, which compiles sources with the processor.
    - Add example sources to `test/resources`, plus expected generated
    sources.
    - Add `ImmutableProcessorTest`, which only verifies that the compilation
    succeeds.

---
+++ immutable-processor/src/test/resources/generated/test/ImmutableEmpty.java
@@ -0,0 +1,10 @@
+package test;
+
+import javax.annotation.processing.Generated;
+
+@Generated("org.example.immutable.processor.ImmutableProcessor")
+class ImmutableEmpty implements Empty {
+
+    ImmutableEmpty() {
+    }
+}
