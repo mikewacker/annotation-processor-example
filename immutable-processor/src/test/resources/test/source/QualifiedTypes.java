d1dda09139f9cdc357841abd56cec6ff021bf7db
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2022-12-02 01:25:16 -0700

    add support for generic interfaces (#14)
    Overview
    
    - Generic interfaces are now supported.
    
    main
    
    - Update `ImmutableTypes` to support generic interfaces.
    
    test
    
    - Update tests for `ImmutableTypes` (and `ImmutableProcessor`).
    - (misc) Add end-to-end test for `QualifiedTypes.java`.

---
+++ immutable-processor/src/test/resources/test/source/QualifiedTypes.java
@@ -0,0 +1,19 @@
+package test.source;
+
+import org.example.immutable.Immutable;
+
+@Immutable
+public interface QualifiedTypes {
+
+    String member1();
+
+    java.lang.String member2();
+
+    Generated member3();
+}
+
+interface String {}
+
+interface Generated {}
+
+interface Override {}
